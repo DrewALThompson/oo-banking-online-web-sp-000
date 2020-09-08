f1b3d359e07df7abab5d649573db10e1b3ee370d
Drew Thompson <drewallynthompson@gmail.com>
2020-09-08 02:53:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,26 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  
+  def initialize(sender, receiver, status="pending", amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+  
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false 
+  end
+  
+  def execute_transaction
+    if !(self.valid? && @sender.balance >= @amount)
+    end
+  end
+      
+      
+  
+  
+  
 end
